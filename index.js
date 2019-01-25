ff1c48ffbdf9852ae8ea374df3ae5e8003cdf0d3
fengfancky <fengfancky@gmail.com>
2019-01-25 15:42:43 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var ConnectDB = require('./db_connect')
 
 connect = new ConnectDB();
-connect.queryData("Select * From class_table");
+var res = connect.queryData("Select * From class_table");
+console.log(res);
