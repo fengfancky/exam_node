4c41c655d3f58702e414f438788d7812dc2e23a6
fengfancky <fengfancky@gmail.com>
2019-01-25 16:20:03 +0800

    update return

--- db_connect.js
+++ db_connect.js
@@ -21,8 +21,9 @@ function ConnectDB(){
                 }
                
             }
+            return res;
         });
-        return res;
+        
     };
 }
 
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var ConnectDB = require('./db_connect')
 
 connect = new ConnectDB();
-var res = connect.queryData("Select * From class_table");
-console.log(res);
+console.log(connect.queryData("Select * From class_table"));
